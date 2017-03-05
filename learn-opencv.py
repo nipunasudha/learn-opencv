10607a14bb1a1b767637edcbc0640c39f677de7e
Nipuna Sudharaka <nipuna.14@cse.mrt.ac.lk>
2017-03-05 23:07:27 +0530

    Add converting webcam feed to B&W and displaying

--- learn-opencv.py
+++ learn-opencv.py
@@ -1,8 +1,21 @@
 import numpy as np
 import cv2
-from matplotlib import pyplot as plt
 
-img = cv2.imread('samples/03.PNG',0)
-plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
-plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
-plt.show()
+cap = cv2.VideoCapture(0)
+cap.set(3, 320) # these are optional
+cap.set(4, 240)
+while (True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Display the resulting frame
+    cv2.imshow('frame', gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
