5363e8079cc88131c72c1ef3354cece11b29133f
Nipuna Sudharaka <nipuna.14@cse.mrt.ac.lk>
2017-03-05 23:44:40 +0530

    Add drawing shapes

--- OpenCV-notes.py
+++ OpenCV-notes.py
@@ -84,15 +84,28 @@ cap.release()
 cv2.destroyAllWindows()
 
 # ============================================================================
-#
-
-
-# ============================================================================
-#
-
+# Create a black image
+img = np.zeros((512, 512, 3), np.uint8)
 
 # ============================================================================
-#
+# DRAWING SHAPES ON AN IMAGE
+# Create a black image
+img = np.zeros((512, 512, 3), np.uint8)
+# Draw a diagonal blue line with thickness of 5 px
+img = cv2.line(img, (0, 0), (511, 511), (255, 0, 0), 5)
+img = cv2.rectangle(img, (384, 0), (510, 128), (0, 255, 0), 3)
+img = cv2.circle(img, (447, 63), 63, (0, 0, 255), -1)
+img = cv2.ellipse(img, (256, 256), (100, 50), 0, 0, 180, 255, -1)
+# polygon drawing
+pts = np.array([[10, 5], [20, 30], [70, 20], [50, 10]], np.int32)
+pts = pts.reshape((-1, 1, 2))
+img = cv2.polylines(img, [pts], True, (0, 255, 255))
+
+cv2.imshow("Drawing", img)
+cv2.waitKey(0)
+
+# ============================================================================
+
 
 
 # ============================================================================
--- learn-opencv.py
+++ learn-opencv.py
@@ -1,21 +1,17 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
-cap.set(3, 320) # these are optional
-cap.set(4, 240)
-while (True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+# Create a black image
+img = np.zeros((512, 512, 3), np.uint8)
+# Draw a diagonal blue line with thickness of 5 px
+img = cv2.line(img, (0, 0), (511, 511), (255, 0, 0), 5)
+img = cv2.rectangle(img,(384,0),(510,128),(0,255,0),3)
+img = cv2.circle(img,(447,63), 63, (0,0,255), -1)
+img = cv2.ellipse(img,(256,256),(100,50),0,0,180,255,-1)
+#polygon drawing
+pts = np.array([[10,5],[20,30],[70,20],[50,10]], np.int32)
+pts = pts.reshape((-1,1,2))
+img = cv2.polylines(img,[pts],True,(0,255,255))
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Display the resulting frame
-    cv2.imshow('frame', gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+cv2.imshow("Drawing", img)
+cv2.waitKey(0)
