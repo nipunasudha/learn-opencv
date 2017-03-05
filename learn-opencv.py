c9aa2236ec76bda7e6087b530ee6349445cbe9cf
Nipuna Sudharaka <nipuna.14@cse.mrt.ac.lk>
2017-03-05 23:51:45 +0530

    Add text on images

--- learn-opencv.py
+++ learn-opencv.py
@@ -5,13 +5,16 @@ import cv2
 img = np.zeros((512, 512, 3), np.uint8)
 # Draw a diagonal blue line with thickness of 5 px
 img = cv2.line(img, (0, 0), (511, 511), (255, 0, 0), 5)
-img = cv2.rectangle(img,(384,0),(510,128),(0,255,0),3)
-img = cv2.circle(img,(447,63), 63, (0,0,255), -1)
-img = cv2.ellipse(img,(256,256),(100,50),0,0,180,255,-1)
-#polygon drawing
-pts = np.array([[10,5],[20,30],[70,20],[50,10]], np.int32)
-pts = pts.reshape((-1,1,2))
-img = cv2.polylines(img,[pts],True,(0,255,255))
+img = cv2.rectangle(img, (384, 0), (510, 128), (0, 255, 0), 3)
+img = cv2.circle(img, (447, 63), 63, (0, 0, 255), -1)
+img = cv2.ellipse(img, (256, 256), (100, 50), 0, 0, 180, 255, -1)
+# polygon drawing
+pts = np.array([[10, 5], [20, 30], [70, 20], [50, 10]], np.int32)
+pts = pts.reshape((-1, 1, 2))
+img = cv2.polylines(img, [pts], True, (0, 255, 255))
+# adding text to an image
+font = cv2.FONT_HERSHEY_SIMPLEX
+cv2.putText(img, 'OpenCV', (10, 500), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
 
 cv2.imshow("Drawing", img)
 cv2.waitKey(0)
