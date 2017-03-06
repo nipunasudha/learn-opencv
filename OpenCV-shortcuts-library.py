3ad47b8d590c0324b1e926c0bf25d291e61146af
Nipuna Sudharaka <nipuna.14@cse.mrt.ac.lk>
2017-03-06 19:05:29 +0530

    Add threshold methods (simple, adaptive, otsu's)

--- OpenCV-shortcuts-library.py
+++ OpenCV-shortcuts-library.py
@@ -250,15 +250,89 @@ print(hsv_green)
 # [[[ 60 255 255]]]
 
 # ============================================================================
-#
+# SIMPLE THRESHOLDING
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
 
+img = cv2.imread('processing/bookpage.png', 0)
+img = cv2.medianBlur(img, 5)
+
+ret, th1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+th2 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, \
+                            cv2.THRESH_BINARY, 11, 2)
+th3 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, \
+                            cv2.THRESH_BINARY, 11, 2)
+
+titles = ['Original Image', 'Global Thresholding (v = 127)',
+          'Adaptive Mean Thresholding', 'Adaptive Gaussian Thresholding']
+images = [img, th1, th2, th3]
+
+for i in range(4):
+    plt.subplot(2, 2, i + 1), plt.imshow(images[i], 'gray')
+    plt.title(titles[i])
+    plt.xticks([]), plt.yticks([])
+plt.show()
 
 # ============================================================================
-#
+# ADAPTIVE THRESHOLDING
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
 
+img = cv2.imread('processing/bookpage.png', 0)
+img = cv2.medianBlur(img, 5)
+
+ret, th1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+th2 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, \
+                            cv2.THRESH_BINARY, 11, 2)
+th3 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, \
+                            cv2.THRESH_BINARY, 11, 2)
+
+titles = ['Original Image', 'Global Thresholding (v = 127)',
+          'Adaptive Mean Thresholding', 'Adaptive Gaussian Thresholding']
+images = [img, th1, th2, th3]
+
+for i in range(4):
+    plt.subplot(2, 2, i + 1), plt.imshow(images[i], 'gray')
+    plt.title(titles[i])
+    plt.xticks([]), plt.yticks([])
+plt.show()
 
 # ============================================================================
-#
+# OTSU'S BINARIZATION
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('processing/bookpage.png', 0)
+
+# global thresholding
+ret1, th1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+
+# Otsu's thresholding
+ret2, th2 = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+# Otsu's thresholding after Gaussian filtering
+blur = cv2.GaussianBlur(img, (5, 5), 0)
+ret3, th3 = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+# plot all the images and their histograms
+images = [img, 0, th1,
+          img, 0, th2,
+          blur, 0, th3]
+titles = ['Original Noisy Image', 'Histogram', 'Global Thresholding (v=127)',
+          'Original Noisy Image', 'Histogram', "Otsu's Thresholding",
+          'Gaussian filtered Image', 'Histogram', "Otsu's Thresholding"]
+
+for i in range(3):
+    plt.subplot(3, 3, i * 3 + 1), plt.imshow(images[i * 3], 'gray')
+    plt.title(titles[i * 3]), plt.xticks([]), plt.yticks([])
+    plt.subplot(3, 3, i * 3 + 2), plt.hist(images[i * 3].ravel(), 256)
+    plt.title(titles[i * 3 + 1]), plt.xticks([]), plt.yticks([])
+    plt.subplot(3, 3, i * 3 + 3), plt.imshow(images[i * 3 + 2], 'gray')
+    plt.title(titles[i * 3 + 2]), plt.xticks([]), plt.yticks([])
+plt.show()
 
 
 # ============================================================================
--- learn-opencv.py
+++ learn-opencv.py
@@ -1,34 +1,22 @@
-
-# img = cv2.imread("processing/bluedot.jpg")
-
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
 
-cap = cv2.VideoCapture(0)
+img = cv2.imread('processing/bookpage.png',0)
+img = cv2.medianBlur(img,5)
 
-while(1):
+ret,th1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+th2 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
+            cv2.THRESH_BINARY,11,2)
+th3 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+            cv2.THRESH_BINARY,11,2)
 
-    # Take each frame
-    _, frame = cap.read()
+titles = ['Original Image', 'Global Thresholding (v = 127)',
+            'Adaptive Mean Thresholding', 'Adaptive Gaussian Thresholding']
+images = [img, th1, th2, th3]
 
-    # Convert BGR to HSV
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-    # define range of blue color in HSV
-    lower_blue = np.array([90,50,50])
-    upper_blue = np.array([150,255,255])
-
-    # Threshold the HSV image to get only blue colors
-    mask = cv2.inRange(hsv, lower_blue, upper_blue)
-
-    # Bitwise-AND mask and original image
-    res = cv2.bitwise_and(frame,frame, mask= mask)
-
-    cv2.imshow('frame',frame)
-    cv2.imshow('mask',mask)
-    cv2.imshow('res',res)
-    k = cv2.waitKey(5) & 0xFF
-    if k == 27:
-        break
-
-cv2.destroyAllWindows()
+for i in range(4):
+    plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
+    plt.title(titles[i])
+    plt.xticks([]),plt.yticks([])
+plt.show()
