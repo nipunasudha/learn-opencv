7a72e3a36dcd65ddf5797d2516d8feb073fc5d8b
Nipuna Sudharaka <nipuna.14@cse.mrt.ac.lk>
2017-03-07 01:09:42 +0530

    Add Canny Edge Detection

--- learn-opencv.py
+++ learn-opencv.py
@@ -1,20 +1,18 @@
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
 
-img = cv2.imread('samples/16.PNG',0)
+cap = cv2.VideoCapture(1)
 
-laplacian = cv2.Laplacian(img,cv2.CV_64F)
-sobelx = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5)
-sobely = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=5)
+while (1):
+    # Take each frame
+    _, frame = cap.read()
 
-plt.subplot(2,2,1),plt.imshow(img,cmap = 'gray')
-plt.title('Original'), plt.xticks([]), plt.yticks([])
-plt.subplot(2,2,2),plt.imshow(laplacian,cmap = 'gray')
-plt.title('Laplacian'), plt.xticks([]), plt.yticks([])
-plt.subplot(2,2,3),plt.imshow(sobelx,cmap = 'gray')
-plt.title('Sobel X'), plt.xticks([]), plt.yticks([])
-plt.subplot(2,2,4),plt.imshow(sobely,cmap = 'gray')
-plt.title('Sobel Y'), plt.xticks([]), plt.yticks([])
+    edges = cv2.Canny(frame, 100, 300)
 
-plt.show()
+    cv2.imshow('frame', frame)
+    cv2.imshow('edges', edges)
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
+        break
+
+cv2.destroyAllWindows()
