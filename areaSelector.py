9672c6033b0d99468e5f49bd1fcaaa4bd8544446
Nipuna Sudharaka <nipuna.14@cse.mrt.ac.lk>
2017-03-15 13:37:46 +0530

    Add selection undo, clearer bounding box

--- areaSelector.py
+++ areaSelector.py
@@ -1,10 +1,25 @@
 import cv2
 import sys
+import operator
 
 # Shared variable declarations
 refPts = []
 image = 1
 numSelected = 0
+clone = 1
+
+
+def render_boundingboxes():
+    global image
+    local_clone = image.copy()
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    for index in range(int(len(refPts) / 2)):
+        cv2.rectangle(local_clone, refPts[index * 2], refPts[(index * 2) + 1], (0, 255, 0), 1, lineType=8)
+        cv2.rectangle(local_clone, tuple(map(operator.add, refPts[(index * 2)], (-1, -1))),
+                      tuple(map(operator.add, refPts[(index * 2) + 1], (1, 1))), (0, 0, 0), 1, lineType=4)
+        cv2.putText(local_clone, 'A ' + str(index + 1), tuple(map(operator.add, refPts[(index * 2)], (0, -3))), font,
+                    0.5, (255, 0, 0), 1, cv2.LINE_AA)
+    cv2.imshow("Select areas to monitor", local_clone)
 
 
 def click_handler(event, x, y, flags, pram):
@@ -17,28 +32,30 @@ def click_handler(event, x, y, flags, pram):
             refPts.append((x, y))
     elif (event == cv2.EVENT_LBUTTONUP):
         refPts.append((x, y))
-        cv2.rectangle(image, refPts[numSelected * 2], refPts[(numSelected * 2) + 1], (147, 101, 187), 2, lineType=8)
-        cv2.imshow("image", image)
+        render_boundingboxes()
         numSelected = numSelected + 1
 
 
 def getSelectionsFromImage(img):
-    global image, refPts, numSelected
+    global image, refPts, numSelected, clone
     image = img
     refPts = []  # Reinit refPts
     clone = image.copy()
-    cv2.namedWindow("image")
-    cv2.setMouseCallback("image", click_handler)
-    cv2.imshow("image", image)
+    cv2.namedWindow("Select areas to monitor")
+    cv2.setMouseCallback("Select areas to monitor", click_handler)
+    cv2.imshow("Select areas to monitor", image)
     while True:
-        # cv2.imshow("image", image)
+        # cv2.imshow("Select areas to monitor", image)
         key = cv2.waitKey(1) & 0xFF
         if (key == ord("d")):
             break
         if (key == ord("r")):
-            refPts = refPts[:len(refPts) - 2]
-            numSelected = numSelected - 1
-            image = clone.copy()
+            if ((len(refPts) % 2) == 0):
+                refPts = refPts[:len(refPts) - 2]
+                numSelected = numSelected - 1
+                render_boundingboxes()
+        if (key == 27):
+            return None
     if ((len(refPts) % 2) == 0):
         print(len(refPts) / 2)
         print(refPts)
@@ -54,6 +71,6 @@ def getSelectionsFromImage(img):
     return refPts
 
 
-img = cv2.imread('samples/13.PNG')
-co=getSelectionsFromImage(img)
+img = cv2.imread('samples/17.PNG')
+co = getSelectionsFromImage(img)
 print(co)
